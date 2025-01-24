e56252c819c2f13d13ec1e11633fb97fcbed8897
MensahPrince <burnszephyr520@gmail.com>
2025-01-24 18:33:01 +0000

    Added Product models and tweaked views

--- web/models.py
+++ web/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 class Product(models.Model):
-    image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=200)
-    price = models.FloatField()
-    stock = models.IntegerField()
-    image_url = models.CharField(max_length=2083)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(default='fallback.png', blank=True)
+
+    def __str__(self):
+        return self.name
