d0b79827284e52c696d88e41d48750b52452395a
MensahPrince <burnszephyr520@gmail.com>
2025-01-24 15:53:49 +0000

    Working on back end

--- web/models.py
+++ web/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=200)
     price = models.FloatField()
     stock = models.IntegerField()
